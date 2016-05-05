d85d6a5de00b14a26bf004372d866b5cd8542b4b
Michael Pimlott <pettingbutterscotch@gmail.com>
2016-05-05 14:19:42 -0700

    add navbar

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ angular.module('redditClone', [])
   $scope.view = {};
   $scope.view.posts = [
     {
-      title: 
-      author:
-      imglink:
-      description:
-      votes:
-      comments:
+      title: "fake title",
+      author: "Jojo Mctoots",
+      imglink: "http://toponehitwonders.com/wp-content/uploads/2010/10/StayingAlivetravolta2.jpg",
+      description: "say something here",
+      votes: "0",
+      comments: []
     }
   ]
 });
