ea295dcfd6c18d6c1d0632efef3cc8deca55fdd0
Michael Pimlott <pettingbutterscotch@gmail.com>
2016-05-05 13:55:18 -0700

    initial commit, set up controller with index.html

---
+++ app.js
@@ -0,0 +1,15 @@
+angular.module('redditClone', [])
+.controller('funTimeController', function($scope) {
+  $scope.it = "good old fashioned jerky";
+  $scope.view = {};
+  $scope.view.posts = [
+    {
+      title: 
+      author:
+      imglink:
+      description:
+      votes:
+      comments:
+    }
+  ]
+});
