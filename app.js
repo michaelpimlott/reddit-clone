ce539060816b7141a775ae0d8c1df27cd304bd5c
Michael Pimlott <pettingbutterscotch@gmail.com>
2016-05-05 17:01:22 -0700

    add title, description, authour to posts section, fix imgs

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 angular.module('redditClone', [])
 .controller('funTimeController', function($scope) {
-  $scope.it = "good old fashioned jerky";
+  $scope.newPost = {};
   $scope.view = {};
   $scope.view.posts = [
     {
-      title: "fake title",
-      author: "Jojo Mctoots",
-      imglink: "http://toponehitwonders.com/wp-content/uploads/2010/10/StayingAlivetravolta2.jpg",
+      title: "We are the worst",
+      author: "a rock",
+      imglink: "http://www.geekologie.com/2008/11/24/naked-mcdonalds.jpg",
       description: "say something here",
       votes: "0",
       comments: []
     }
+    // {
+    //   title: "real title",
+    //   author: "buster-ass mark",
+    //   imglink: "http://www.geekologie.com/2008/11/24/naked-mcdonalds.jpg",
+    //   description: "I too say stuff",
+    //   votes: "0",
+    //   comments: []
+    // }
   ]
 });
