7814fef153684631d268e57a6eb9df12c3d023ed
Michael Pimlott <pettingbutterscotch@gmail.com>
2016-07-28 16:09:29 -0700

    clean up code in index.html, add to ReadME

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # reddit-clone
+
+Angular post and vote site in the style of Reddit.
--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 angular.module('redditClone', [])
-.controller('funTimeController', function($scope) {
-  $scope.newPost = {};
-  $scope.newComment = {};
-  $scope.view = {};
-  $scope.view.newPostSee = false;
-  $scope.view.sortOptions = ['date', 'votes', 'title'];
-  $scope.view.defaultSort = $scope.view.sortOptions[0];
-  $scope.view.valueOf = "-votes";
-  $scope.view.search = "";
-  $scope.view.posts = [
-    {
+  .controller('funTimeController', function($scope) {
+    $scope.newPost = {};
+    $scope.newComment = {};
+    $scope.view = {};
+    $scope.view.newPostSee = false;
+    $scope.view.sortOptions = ['date', 'votes', 'title'];
+    $scope.view.defaultSort = $scope.view.sortOptions[2];
+    $scope.view.valueOf = "-votes";
+    $scope.view.search = "";
+    $scope.view.posts = [{
       title: "We are the worst",
       author: "a rock",
       imglink: "http://photos1.blogger.com/blogger/2573/1907/1600/stayingalive01.jpg",
@@ -17,84 +16,85 @@ angular.module('redditClone', [])
       votes: 0,
       comments: [],
       commentSee: false,
-      newCommentSee:false
+      newCommentSee: false
 
-    },
-    {
+    }, {
       title: "real title",
       author: "buster-ass mark",
       imglink: "http://www.polyvore.com/cgi/img-thing?.out=jpg&size=l&tid=15055139",
       description: "Brunch street art blue bottle forage man bun narwhal. Pitchfork crucifix biodiesel echo park, direct trade williamsburg shabby chic. Gochujang chia photo booth everyday carry, put a bird on it affogato portland viral cornhole try-hard art party whatever migas sartorial. Asymmetrical four loko PBR&B, wolf 3 wolf moon butcher austin DIY sustainable put a bird on it seitan master cleanse locavore. Cred quinoa freegan photo booth drinking vinegar meggings, four dollar toast put a bird on it fap artisan. Kombucha authentic hoodie listicle etsy umami wayfarers. Gastropub franzen vice, 3 wolf moon pitchfork selfies tattooed health goth.",
       votes: 0,
-      comments: []
+      comments: [],
+      commentSee: false,
+      newCommentSee: false
 
-    },
-    {
+    }, {
       title: "Something else",
       author: "Edward James Almost",
       imglink: "http://www.olmosperfect.com/op-images/stand-and-deliver-thumb.jpg",
       description: "Brunch street art blue bottle forage man bun narwhal. Pitchfork crucifix biodiesel echo park, direct trade williamsburg shabby chic. Gochujang chia photo booth everyday carry, put a bird on it affogato portland viral cornhole try-hard art party whatever migas sartorial. Asymmetrical four loko PBR&B, wolf 3 wolf moon butcher austin DIY sustainable put a bird on it seitan master cleanse locavore. Cred quinoa freegan photo booth drinking vinegar meggings, four dollar toast put a bird on it fap artisan. Kombucha authentic hoodie listicle etsy umami wayfarers. Gastropub franzen vice, 3 wolf moon pitchfork selfies tattooed health goth.",
       votes: 0,
-      comments: []
+      comments: [],
+      commentSee: false,
+      newCommentSee: false
 
-    }
-  ];
+    }];
 
-  $scope.setSortOrder = function(newValueOf) {
-    $scope.view.defaultSort = newValueOf;
-    $scope.view.valueOf = newValueOf === "title" ? newValueOf : '-' + newValueOf;
-  };
+    $scope.setSortOrder = function(newValueOf) {
+      $scope.view.defaultSort = newValueOf;
+      $scope.view.valueOf = newValueOf === "title" ? newValueOf : '-' + newValueOf;
+    };
 
-  $scope.toggleNewPostSee = function(post) {
+    $scope.toggleNewPostSee = function(post) {
       $scope.view.newPostSee = !$scope.view.newPostSee;
-  };
+    };
 
-  $scope.toggleCommentSee = function(post) {
-    post.commentsSee = !post.commentsSee;
-  }
-
-  $scope.toggleNewCommentSee = function(post) {
-    $scope.view.posts.forEach(function(oldPost){
-      if (oldPost !==post){
-        oldPost.newCommentSee = false;
-      } else {
-        oldPost.newCommentSee = !oldPost.newCommentSee;
+    $scope.toggleCommentSee = function(post) {
+      post.commentsSee = !post.commentsSee;
     }
-  });
-    $scope.newComment = {};
-  };
 
-
-  $scope.addComment = function(post, comment) {
-    if (comment.author && coment.text) {
-      post.comments.push(comment);
-      post.newCommentSee = false;
+    $scope.toggleNewCommentSee = function(post) {
+      $scope.view.posts.forEach(function(oldPost) {
+        if (oldPost !== post) {
+          oldPost.newCommentSee = false;
+        } else {
+          oldPost.newCommentSee = !oldPost.newCommentSee;
+        }
+      });
       $scope.newComment = {};
-    }
-  };
+    };
 
-  $scope.addPost = function(post) {
-    post.votes = 0;
-    post.comments = [];
-    post.commentsSee = false;
-    $scope.view.posts.push(post);
-    $scope.view.newPostSee = false;
-    $scope.newPost = {};
-    $scope.postForm.$setUntouched();
 
-  };
+    $scope.addComment = function(post, comment) {
+      if (comment.author && comment.text) {
+        post.comments.push(comment);
+        post.newCommentSee = false;
+        $scope.newComment = {};
+      }
+    };
 
-  $scope.voteChange = function(post, changeValue) {
-    post.votes += changeValue;
-  };
+    $scope.addPost = function(post) {
+      post.votes = 0;
+      post.comments = [];
+      post.commentsSee = false;
+      $scope.view.posts.push(post);
+      $scope.view.newPostSee = false;
+      $scope.newPost = {};
+      $scope.postForm.$setUntouched();
 
-  $scope.changeColorClass = function(post) {
-    if (post.votes > 0) {
-      return "positive";
-    } else if (post.votes < 0) {
-      return "negative";
-    } else {
-      return "";
-    }
-  };
-});
+    };
+
+    $scope.voteChange = function(post, changeValue) {
+      post.votes += changeValue;
+    };
+
+    $scope.changeColorClass = function(post) {
+      if (post.votes > 0) {
+        return "positive";
+      } else if (post.votes < 0) {
+        return "negative";
+      } else {
+        return "";
+      }
+    };
+  });
